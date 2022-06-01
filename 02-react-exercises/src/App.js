513f8935a51ec9cce33a637e8b927c38c7002817
Duk3nd3 <hjmv1982@outlook.com.ar>
2022-05-31 23:43:52 -0300

    Selects Anidados: Renderizado de datos y estilos CSS en React JS

--- 02-react-exercises/src/App.js
+++ 02-react-exercises/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import CrudApi from './components/CrudApi';
 import CrudApp from './components/CrudApp';
+import SelectAnidados from './components/SelectAnidados';
 import SongSearch from './components/SongSearch';
 
 function App() {
 	return (
 		<>
 			<h1>Ejercicios con React</h1>
+			<hr />
+			<SelectAnidados />
 			<hr />
 			<SongSearch />
 			<hr />
