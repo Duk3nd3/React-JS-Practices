091e522e8a8cf17a4719f630277d1a8540ff5d70
Duk3nd3 <hjmv1982@outlook.com.ar>
2022-06-03 21:43:24 -0300

    Ventana Modal: Estilos y logica del componente + Search Song Modal (Finalizado)

--- 02-react-exercises/src/components/Modals.jsx
+++ 02-react-exercises/src/components/Modals.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
 import { useModal } from '../hooks/useModal';
+import ContactForm from './ContactForm';
 import { Modal } from './Modal';
+import SongSearch from './SongSearch';
 
 const Modals = () => {
 	const [isOpenModalUno, openModalUno, closeModalUno] = useModal(false);
 	const [isOpenModalDos, openModalDos, closeModalDos] = useModal(false);
+	const [isOpenContact, openModalContact, closeModalContact] = useModal(false);
+	const [isOpenSearchSong, openSearchSong, closeSearchSong] = useModal(false);
 
 	return (
 		<div>
 			<h2>Modales</h2>
-			<button>Modal 1</button>
-			<Modal>
+			<button onClick={openModalUno}>Modal 1</button>
+			<Modal isOpen={isOpenModalUno} closeModal={closeModalUno}>
 				<h3>Modal 1</h3>
 				<p>Hola este es el contenido de mi modal 1</p>
 				<img src='https://placeimg.com/400/400/animals' alt='Animals' />
 			</Modal>
-			<button>Modal 2</button>
-			{/* <Modal>
+			<button onClick={openModalDos}>Modal 2</button>
+			<Modal isOpen={isOpenModalDos} closeModal={closeModalDos}>
 				<h3>Hola este es el contenido de mi modal 2</h3>
 				<p>
 					Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda,
@@ -24,7 +28,15 @@ const Modals = () => {
 					tempore sit explicabo consectetur totam, voluptatum numquam esse?
 				</p>
 				<img src='https://placeimg.com/400/400/nature' alt='Nature' />
-			</Modal> */}
+			</Modal>
+			<button onClick={openModalContact}>Modal Contacto</button>
+			<Modal isOpen={isOpenContact} closeModal={closeModalContact}>
+				<ContactForm />
+			</Modal>
+			<button onClick={openSearchSong}>Modal Search Song</button>
+			<Modal isOpen={isOpenSearchSong} closeModal={closeSearchSong}>
+				<SongSearch />
+			</Modal>
 		</div>
 	);
 };
