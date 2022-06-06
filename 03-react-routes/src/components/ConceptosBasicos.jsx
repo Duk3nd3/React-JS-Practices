0a9708b1b8e9141fa0da751366fa695490e08437
Duk3nd3 <hjmv1982@outlook.com.ar>
2022-06-06 20:47:56 -0300

    React Routes: Diferentes tipos de rutas RR @5.2.0

--- 03-react-routes/src/components/ConceptosBasicos.jsx
+++ 03-react-routes/src/components/ConceptosBasicos.jsx
@@ -12,9 +12,31 @@ const ConceptosBasicos = () => {
 					<h3>Home</h3>
 					<p>Bienvenid@s al tema de las rutas en React</p>
 				</Route>
-				<Route exact path='/acerca' component={Acerca} />
+				<Route exact path='/acerca'>
+					<Acerca />
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit.
+						Consequuntur commodi alias cupiditate accusantium earum atque minus
+						sunt iste iure porro doloremque corrupti nisi laudantium,
+						necessitatibus dolore odit amet! Earum, distinctio!
+					</p>
+				</Route>
 				{/* <Route exact path='/contacto' component={Contacto} /> */}
-				<Route exact path='/contacto' children={<Contacto />} />
+				<Route
+					exact
+					path='/contacto'
+					children={
+						<>
+							<Contacto />
+							<p>
+								Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+								molestiae culpa saepe eligendi quod excepturi mollitia aperiam
+								nostrum veniam odio labore ab distinctio iure, non magni soluta
+								vitae inventore nisi.
+							</p>
+						</>
+					}
+				/>
 			</Router>
 		</div>
 	);
