d0405e7951c43131d03cb44876e7e851ac9e22e5
Duk3nd3 <hjmv1982@outlook.com.ar>
2022-06-11 17:42:37 -0300

    Finalizacion con RUTAS en React

--- 03-react-routes/src/App.js
+++ 03-react-routes/src/App.js
@@ -1,5 +1,5 @@
-// import ConceptosBasicos from './components/ConceptosBasicos';
-// import CrudApi from './components/CrudApi';
+import ConceptosBasicos from './components/ConceptosBasicos';
+import CrudApi from './components/CrudApi';
 import SongSearch from './components/SongSearch';
 
 function App() {
@@ -16,9 +16,9 @@ function App() {
 			<hr />
 			<SongSearch />
 			<hr />
-			{/* <CrudApi /> */}
+			<CrudApi />
 			<hr />
-			{/* <ConceptosBasicos /> */}
+			<ConceptosBasicos />
 		</>
 	);
 }
