4c0207a5dff8d0c7874153e9a9965caa624363f4
Duk3nd3 <hjmv1982@outlook.com.ar>
2022-06-20 20:20:53 -0300

    Carrito de Compras con Reducers (2 / 5)

--- 06-react-reducers/src/components/ShoppingCart.jsx
+++ 06-react-reducers/src/components/ShoppingCart.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+import {
+	shoppingReducer,
+	shoppingInitialState,
+} from '../reducers/ShoppingReducer';
+import ProductItem from './ProductItem';
 
 const ShoppingCart = () => {
+	const [state, dispatch] = useReducer(shoppingReducer, shoppingInitialState);
+
+	const { products, cart } = state;
+
+	const addToCart = (id) => {
+		console.log(id);
+	};
+
+	const delFromCart = () => {};
+
+	const clearCart = () => {};
+
 	return (
 		<div>
 			<h2>Carrito de Compras</h2>
 			<h3>Productos</h3>
-			<article className='box'></article>
+			<article className='box grid-responsive'>
+				{products.map((product) => (
+					<ProductItem key={product.id} data={product} addToCart={addToCart} />
+				))}
+			</article>
 			<h3>Carrito</h3>
 			<article className='box'></article>
 		</div>
--- 06-react-reducers/src/reducers/ShoppingReducer.jsx
+++ 06-react-reducers/src/reducers/ShoppingReducer.jsx
@@ -1,6 +1,6 @@
 import { TYPES } from '../actions/ShoppingAction';
 
-export const shopingInitialState = {
+export const shoppingInitialState = {
 	products: [
 		{ id: 1, name: 'Producto 1', price: 100 },
 		{ id: 2, name: 'Producto 2', price: 200 },
