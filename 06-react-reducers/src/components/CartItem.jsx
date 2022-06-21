c83f7b430c958e966ab3b94e7927cd14720dfe5b
Duk3nd3 <hjmv1982@outlook.com.ar>
2022-06-20 21:47:58 -0300

    Carrito de Compras con Reducers ( 4 / 5 )

--- 06-react-reducers/src/components/CartItem.jsx
+++ 06-react-reducers/src/components/CartItem.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
 const CartItem = ({ data, delFromCart }) => {
-	let { id, name, price } = data;
+	let { id, name, price, quantity } = data;
 
 	return (
 		<div style={{ borderBottom: 'thin solid gray' }}>
 			<h4>{name}</h4>
-			<h5>${price}.00</h5>
-			<button onClick={delFromCart}>Eliminar</button>
+			<h5>
+				${price}.00 x {quantity} = ${price * quantity}.00
+			</h5>
+			<button>Eliminar</button>
+			<button>Eliminar todo</button>
 		</div>
 	);
 };
--- 06-react-reducers/src/reducers/ShoppingReducer.jsx
+++ 06-react-reducers/src/reducers/ShoppingReducer.jsx
@@ -18,11 +18,22 @@ export function shoppingReducer(state, action) {
 			let newItem = state.products.find(
 				(product) => product.id === action.payload
 			);
-			// console.log(newItem);
-			return {
-				...state,
-				cart: [...state.cart, newItem],
-			};
+
+			let itemInCart = state.cart.find((item) => item.id === newItem.id);
+
+			return itemInCart
+				? {
+						...state,
+						cart: state.cart.map((item) =>
+							item.id === newItem.id
+								? { ...item, quantity: item.quantity + 1 }
+								: item
+						),
+				  }
+				: {
+						...state,
+						cart: [...state.cart, { ...newItem, quantity: 1 }],
+				  };
 		}
 		case TYPES.REMOVE_ONE_FROM_CART: {
 		}
